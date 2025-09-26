551df85d8e8168cb52340d393c8091321eedc5f1
45673449-wq <45673449@continental.edu.pe>
2025-09-25 22:24:12 -0500

    Update test_items.py

--- tests/test_items.py
+++ tests/test_items.py
@@ -2,10 +2,13 @@ from src.models import Session, Item
 
 def test_insert_and_query_item():
     session = Session()
-    new_item = Item(name="Cuaderno", description="Cuadriculado")
+
+    # Insertar un item
+    new_item = Item(name="Borrador", description="Blanco")
     session.add(new_item)
     session.commit()
 
-    saved = session.query(Item).filter_by(name="Cuaderno").first()
+    # Consultar el item
+    saved = session.query(Item).filter_by(name="Borrador").first()
     assert saved is not None
-    assert saved.description == "Cuadriculado"
+    assert saved.description == "Blanco"
