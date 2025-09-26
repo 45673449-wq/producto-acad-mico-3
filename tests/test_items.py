0b6aa5158f2adc2301bf383868bc5825d7a0ea91
45673449-wq <45673449@continental.edu.pe>
2025-09-25 22:18:37 -0500

    Update test_items.py

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,18 +1,11 @@
-# tests/test_items.py
-from src.models import get_engine, init_db, get_session, Item
+from src.models import Session, Item
 
 def test_insert_and_query_item():
-    # Usamos una BD en memoria para no tocar archivos en disco
-    engine = get_engine("sqlite:///:memory:")
-    init_db(engine)                 # crea las tablas en la BD en memoria
-    session = get_session(engine)   # sesión ligada a ese engine
-
-    # insertar
-    new_item = Item(name="Borrador", description="Blanco")
+    session = Session()
+    new_item = Item(name="Cuaderno", description="Cuadriculado")
     session.add(new_item)
     session.commit()
 
-    # consultar
-    saved = session.query(Item).filter_by(name="Borrador").first()
+    saved = session.query(Item).filter_by(name="Cuaderno").first()
     assert saved is not None
-    assert saved.description == "Blanco"
+    assert saved.description == "Cuadriculado"
